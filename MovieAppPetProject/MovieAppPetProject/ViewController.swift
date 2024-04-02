042cf1d055ad3d554840145c493c08655a76272e
sebastianj7 <sebastianjacobs2000@gmail.com>
2024-04-02 07:51:02 +0200

    Empty project commit

---
+++ MovieAppPetProject/MovieAppPetProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieAppPetProject
+//
+//  Created by Sebastian Jacobs on 2024/04/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
